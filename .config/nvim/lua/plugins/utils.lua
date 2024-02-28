717419ebd1dbb05cd40c803867afe7f5ad0b348b
Nicklas Stockton <nicklas.stockton@gmail.com>
2024-02-28 11:45:07 -0700

    get lsp completion working

--- .config/nvim/lua/plugins/utils.lua
+++ .config/nvim/lua/plugins/utils.lua
@@ -37,10 +37,53 @@ return {
     'qpkorr/vim-bufkill',
     -- completion with LSP
     {
-        'hrsh7th/cmp-nvim-lsp',
+        -- ripped directly from jonhoo/configs, will customize when I have the time
+        "hrsh7th/nvim-cmp",
+        -- load cmp on InsertEnter
+        event = "InsertEnter",
+        -- these dependencies will only be loaded when cmp loads
+        -- dependencies are always lazy-loaded unless specified otherwise
         dependencies = {
-            'hrsh7th/nvim-cmp',
+            'neovim/nvim-lspconfig',
+            "hrsh7th/cmp-nvim-lsp",
+            "hrsh7th/cmp-buffer",
+            "hrsh7th/cmp-path",
         },
+        config = function()
+            local cmp = require'cmp'
+            cmp.setup({
+                snippet = {
+                    -- REQUIRED by nvim-cmp. get rid of it once we can
+                    expand = function(args)
+                        vim.fn["vsnip#anonymous"](args.body)
+                    end,
+                },
+                mapping = cmp.mapping.preset.insert({
+                    ['<C-b>'] = cmp.mapping.scroll_docs(-4),
+                    ['<C-f>'] = cmp.mapping.scroll_docs(4),
+                    ['<C-Space>'] = cmp.mapping.complete(),
+                    ['<C-e>'] = cmp.mapping.abort(),
+                    -- Accept currently selected item.
+                    -- Set `select` to `false` to only confirm explicitly selected items.
+                    ['<CR>'] = cmp.mapping.confirm({ select = true }),
+                }),
+                sources = cmp.config.sources({
+                    { name = 'nvim_lsp' },
+                }, {
+                    { name = 'path' },
+                }),
+                experimental = {
+                    ghost_text = true,
+                },
+            })
+
+            -- Enable completing paths in :
+            cmp.setup.cmdline(':', {
+                sources = cmp.config.sources({
+                    { name = 'path' }
+                })
+            })
+        end
     },
     -- open file with cursor restored to last position
     'farmergreg/vim-lastplace',
